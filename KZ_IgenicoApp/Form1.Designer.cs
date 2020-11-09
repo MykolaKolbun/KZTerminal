dc201e282f7c451ce619ecce9d3af24aed0d8098
Administrator <Administrator@DESKTOP-MIMQ8NL>
2020-11-09 16:10:10 +0200

    add class TrposX

--- KZ_IgenicoApp/Form1.Designer.cs
+++ KZ_IgenicoApp/Form1.Designer.cs
@@ -28,20 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tbConnectionString = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPurch = new System.Windows.Forms.Button();
+            this.tbAmount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnSettlement = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(65, 50);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // tbConnectionString
+            // 
+            this.tbConnectionString.Location = new System.Drawing.Point(10, 25);
+            this.tbConnectionString.Name = "tbConnectionString";
+            this.tbConnectionString.Size = new System.Drawing.Size(130, 22);
+            this.tbConnectionString.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Connection String";
+            // 
+            // btnPurch
+            // 
+            this.btnPurch.Location = new System.Drawing.Point(65, 120);
+            this.btnPurch.Name = "btnPurch";
+            this.btnPurch.Size = new System.Drawing.Size(75, 23);
+            this.btnPurch.TabIndex = 3;
+            this.btnPurch.Text = "Purchase";
+            this.btnPurch.UseVisualStyleBackColor = true;
+            // 
+            // tbAmount
+            // 
+            this.tbAmount.Location = new System.Drawing.Point(80, 90);
+            this.tbAmount.Name = "tbAmount";
+            this.tbAmount.Size = new System.Drawing.Size(60, 22);
+            this.tbAmount.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Amount:";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(65, 240);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(170, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(320, 255);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // btnSettlement
+            // 
+            this.btnSettlement.Location = new System.Drawing.Point(65, 175);
+            this.btnSettlement.Name = "btnSettlement";
+            this.btnSettlement.Size = new System.Drawing.Size(75, 23);
+            this.btnSettlement.TabIndex = 8;
+            this.btnSettlement.Text = "Settlement";
+            this.btnSettlement.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(558, 450);
+            this.ClientSize = new System.Drawing.Size(494, 267);
+            this.Controls.Add(this.btnSettlement);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbAmount);
+            this.Controls.Add(this.btnPurch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbConnectionString);
+            this.Controls.Add(this.btnConnect);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox tbConnectionString;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPurch;
+        private System.Windows.Forms.TextBox tbAmount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnSettlement;
     }
 }
 
